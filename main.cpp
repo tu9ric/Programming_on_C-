c76df19a77becb69c6f34a0697fbccac7225e8eb
ark23 <ark23@tpu.ru>
2024-09-06 15:23:05 +0700

    added second string to main for test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main (int, char **) {
-    std::cout << "Version: " << version() << std::endl;
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world! Version: 1.0." << version() << std::endl;
+    std::cout << "Second string" << std::endl;
     return 0;
 }
